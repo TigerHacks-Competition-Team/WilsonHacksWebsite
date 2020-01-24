d035064964161cb15fbfe86416bbfbd3e6b0d6cc
Maxwell Stone <maxwellstone@Maxwells-MacBook-Pro.local>
2020-01-24 15:41:43 -0500

    lower case

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.df45f99e3439e716c1bc6dc2a250144b.js"
+  "/precache-manifest.14212aa87fce1429b1ec46d56055d393.js"
 );
 
 self.addEventListener('message', (event) => {
